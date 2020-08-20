873d301eb83255248d4dbde47d90ee7e7752de9d
Lewis Prescott <Lewis.Prescott@cancer.org.uk>
2020-08-20 09:25:45 +0100

    starting point

--- src/counter.js
+++ src/counter.js
@@ -1,15 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 export const Counter = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => {
-    setCount(count + 1);
-  };
   return (
     <>
-      <span data-testid="count">Count: {count}</span>
-      <button type="button" onClick={increment}>
-        Increment
-      </button>
+      <span data-testid="count"></span>
     </>
   );
 };
--- src/counter.test.js
+++ src/counter.test.js
@@ -4,18 +4,8 @@ import "@testing-library/react/cleanup-after-each";
 import "jest-dom/extend-expect";
 import { Counter } from "./counter";
 
-it("should display the start count as 0", () => {
+it.skip("should display the start count as 0", () => {
   const { getByTestId } = render(<Counter />);
 
   expect(getByTestId("count")).toHaveTextContent("Count: 0");
-});
-
-it("should increase the count by one when the user increments the count", () => {
-  const { getByTestId, getByText } = render(<Counter />);
-
-  fireEvent.click(getByText("Increment"));
-  expect(getByTestId("count")).toHaveTextContent("Count: 1");
-
-  fireEvent.click(getByText("Increment"));
-  expect(getByTestId("count")).toHaveTextContent("Count: 2");
 });
